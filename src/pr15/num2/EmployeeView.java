adb0997a88a330d8adea8da4585084437c80e816
iamdelrey <stepanton94@mail.ru>
2023-10-17 22:25:43 +0300

    init

--- src/pr15/num2/EmployeeView.java
+++ src/pr15/num2/EmployeeView.java
@@ -3,6 +3,6 @@ package pr15.num2;
 public class EmployeeView {
     public void printEmployeeDetails(String name, double salary) {
         System.out.println("Имя сотрудника: " + name);
-        System.out.println("Заработная плата: " + salary);
+        System.out.println("Заработная плата: $" + salary);
     }
 }
